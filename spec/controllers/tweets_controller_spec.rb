e9f91be1867631ed62c2918147af3d0f9a571f09
Carlos <charliedn@Gmail.com>
2017-11-23 15:55:13 +0000

    Tests are fixed

--- spec/controllers/tweets_controller_spec.rb
+++ spec/controllers/tweets_controller_spec.rb
@@ -9,14 +9,7 @@ RSpec.describe TweetsController, type: :controller do
     end
   end
 
-  describe "GET #show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #new" do
+describe "GET #new" do
     it "returns http success" do
       get :new
       expect(response).to have_http_status(:success)
