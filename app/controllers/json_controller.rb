aeb05355ef8163f2a667e987e226f442337741a9
Carlos <charliedn@Gmail.com>
2017-11-23 05:40:49 +0000

    Json view will work even without data to read from

--- app/controllers/json_controller.rb
+++ app/controllers/json_controller.rb
@@ -7,10 +7,17 @@ class JsonController < ApplicationController
       json_data = JSON.parse(@file)
     rescue
       puts "Oops something went wrong reading the file."
+      flash[:notice] = "File is empty. There is no data in the file to be read. Please, send a message first"
       redirect_to messages_path
     end
-    @emails = json_data[0]
-    @tweets = json_data[1]
-    @texts = json_data[2]
+    unless json_data.nil?
+      @emails = json_data[0]
+      @tweets = json_data[1]
+      @texts  = json_data[2]
+    else
+      @emails = "File is empty (lib/messages.txt)"
+      @tweets = "File is empty (lib/messages.txt)"
+      @texts  = "File is empty (lib/messages.txt)"
+    end
   end
 end
